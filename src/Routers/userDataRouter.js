56904f1586c103454699b01e9cce10dba6e23b03
Atishay Jain <hritikjain707@gmail.com>
2021-07-09 15:23:17 +0530

    Refactor: JWt Middleware

--- src/Routers/userDataRouter.js
+++ src/Routers/userDataRouter.js
@@ -11,18 +11,37 @@ const verifyToken = (req, res, next) => {
   jwt.verify(authToken, secret, (err, decoded) => {
     if (err) {
       if (err.name === "TokenExpiredError") {
+        console.log("\n \n \n Token Expired Error ");
         return res.status(401).json({ message: "Session Expired" });
       }
+
+      console.log("\n \n \n Some Error Occured ");
       return res.status(400).json({ message: "Some Error Occured" });
+    } else {
+      console.log("\n \n \n Auth Granted ");
+      res.locals.authenticated = decoded;
+      next();
     }
-    return (res.locals.authenticated = decoded);
   });
 
-  next();
+  // jwt.verify(authToken, secret, (err, decoded) => {
+
+  //   if (err) {
+  //     if (err.name === "TokenExpiredError") {
+  //       return res.status(401).json({ message: "Session Expired" });
+  //     }
+  //     return res.status(400).json({ message: "Some Error Occured" });
+  //   }
+  //   return (res.locals.authenticated = decoded);
+  // });
+
+  // next();
 };
 
 router.post("/userdata", verifyToken, async (req, res) => {
   let authenticated = res.locals.authenticated;
+
+  console.log("\n \n \n Autheticated \n \n \n ", authenticated);
 
   let { username, firstname, lastname, followers, following, posts } =
     await Users.findOne({ username: authenticated.username });
